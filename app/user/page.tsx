5d9820da296c5c9c1df346cd291145f30063c1ef
buggy <nguyendanghoaa@gmail.com>
2023-10-17 10:10:29 +0700

    chore: vessel mng

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -17,31 +17,28 @@ export default function User() {
   }
 
   return (
-    <>
-      <div className="flex-col">
-        <div className="flex-1 space-y-4 p-8 pt-6">
-
-          <Tabs value={active} className="space-y-4">
-            <div className="flex items-center justify-between space-y-2">
-              <div>
-                <h2 className="text-2xl font-bold tracking-tight">User management</h2>
-              </div>
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <Tabs value={active} className="space-y-4">
+          <div className="flex items-center justify-between space-y-2">
+            <div>
+              <h2 className="text-2xl font-bold tracking-tight">User management</h2>
             </div>
-            <TabsList>
-              <TabsTrigger value="all" onClick={() => handleChangeTab('all')}>All</TabsTrigger>
-              <TabsTrigger value="admin" onClick={() => handleChangeTab('admin')}>
-                Admin
-              </TabsTrigger>
-              <TabsTrigger value="user" onClick={() => handleChangeTab('user')}>
-                User
-              </TabsTrigger>
-            </TabsList>
-            <TabsContent value="all" className="space-y-4">
-              <UserTable />
-            </TabsContent>
-          </Tabs>
-        </div>
+          </div>
+          <TabsList>
+            <TabsTrigger value="all" onClick={() => handleChangeTab('all')}>All</TabsTrigger>
+            <TabsTrigger value="admin" onClick={() => handleChangeTab('admin')}>
+              Admin
+            </TabsTrigger>
+            <TabsTrigger value="user" onClick={() => handleChangeTab('user')}>
+              User
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="all" className="space-y-4">
+            <UserTable />
+          </TabsContent>
+        </Tabs>
       </div>
-    </>
+    </div>
   )
 }
--- app/vessel/page.tsx
+++ app/vessel/page.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { DataTable } from './components/data-table'
+import { tasks } from './data/tasks'
+import { columns } from './components/columns'
 
 const Vessel = () => {
   return (
-    <div>Vessel</div>
+    <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
+      <div className="flex items-center justify-between space-y-2">
+        <div>
+          <h2 className="text-2xl font-bold tracking-tight">Vessel management</h2>
+          <p className="text-muted-foreground">
+            Here&apos;s  list of Vessel for you
+          </p>
+        </div>
+      </div>
+      <DataTable data={tasks} columns={columns} />
+    </div>
   )
 }
 
